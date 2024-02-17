516e732cfa705550b06400cc406f2f49934f9286
esepgit <150302936+esepgit@users.noreply.github.com>
2024-02-17 17:07:27 -0300

    8.3: query allAuthors

--- 8a/index.js
+++ 8a/index.js
@@ -106,10 +106,18 @@ const typeDefs = `
     id: ID!
   }
 
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `;
 
@@ -117,8 +125,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
   },
+  Author: {
+    bookCount: (root) => books.filter(b => b.author === root.name).length
+  }
 };
 
 const server = new ApolloServer({
