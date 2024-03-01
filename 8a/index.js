e39713ea911da0577bd922f1b6d93e8115804733
esepgit <150302936+esepgit@users.noreply.github.com>
2024-03-01 16:03:57 -0300

    8.6: add a book mutation

--- 8a/index.js
+++ 8a/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -119,6 +120,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book]
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
 `;
 
 const resolvers = {
@@ -138,6 +148,26 @@ const resolvers = {
   },
   Author: {
     bookCount: (root) => books.filter(b => b.author === root.name).length
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = {
+        ...args, id: uuid()
+      }
+      books = books.concat(book)
+      //agregar author si es que no existe
+      if (!authors.find(a => a.name === args.author)) {
+        authors = authors.concat({
+          name: args.author,
+          id: uuid(),
+          born: null,
+          bookCount: 1
+        })
+       
+      }
+      
+      return book;
+    }
   }
 };
 
